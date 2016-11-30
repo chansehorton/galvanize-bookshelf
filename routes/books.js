d95f56b3dcd2740be404e4072fecc168296a62da
CHANSE HORTON <chansehorton@yahoo.com>
2016-11-30 13:33:40 -0700

    get and get with id working

--- routes/books.js
+++ routes/books.js
@@ -7,28 +7,58 @@ const { camelize, decamelize } = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/books', function (request, response, next) {
-  console.log('GET happened');
-  knex('books')
-    .orderBy('title')
-    .then((result) => {
-      // response.setHeader('Content-Type', 'application/json');
-      response.setHeader('Accept', 'application/json');
-      response.send(result);
-    })
-    .catch((err) => {
-      next(err);
-    });
+//get all or get only the element with the requested id
+// router.get('/books/:id?', function (request, response, next) {
+//   let reqId = parseInt(request.params.id, 10);
+//   knex('books')
+//     .orderBy('title')
+//     .then((result) => {
+//       response.setHeader('Accept', 'application/json');
+//       if (reqId && !(isNaN(reqId))) {
+//         for (let i=0;i<result.length;i++) {
+//           if (result[i].id === reqId) {
+//             response.send(result[i]);
+//           }
+//         }
+//       } else {
+//         response.send(result);
+//       };
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// });
 
+router.get('/books/:id?', function (request, response, next) {
+  let reqId = parseInt(request.params.id, 10);
+
+  response.setHeader('Accept', 'application/json');
+  if (!request.params.id) {
+    knex('books')
+      .orderBy('title')
+      .then((result) => {
+        response.send(result);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  } else if (isNaN(reqId)) {
+    return response.sendStatus(400);
+  } else if (reqId && !(isNaN(reqId))) {
+    knex('books')
+      .where('id', reqId)
+      .then((result) => {
+        response.send(result);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  };
 });
 
-// router.get('/:id', function(request, response, next) {
-//   response.send(`Get User - ${request.params.id}`);
-// });
-//
-// router.post('/', function(request, response, next) {
-//   response.send('Create a User');
-// });
+router.post('/books', function(request, response, next) {
+  
+});
 //
 // router.delete('/:id', function(request, response, next) {
 //   response.send(`Delete User - ${request.params.id}`);
