b490cd88353c319ace663a5811ba00f8ea0c81c2
CHANSE HORTON <chansehorton@yahoo.com>
2016-11-30 16:21:58 -0700

    working patch

--- routes/books.js
+++ routes/books.js
@@ -45,10 +45,68 @@ router.get('/books/:id?', function (request, response, next) {
       });
   } else if (isNaN(reqId)) {
     return response.sendStatus(400);
-  } else if (reqId && !(isNaN(reqId))) {
+  } else if (reqId > 0) {
     knex('books')
       .where('id', reqId)
       .then((result) => {
+        if (Object.keys(result[0]).length === 0) {
+          return response.sendStatus(400);
+        } else {
+          const sendResult = camelizeKeys(result);
+          response.send(sendResult[0]);
+        }
+      })
+      .catch((err) => {
+        next(err);
+      });
+  };
+});
+
+router.post('/books', function(request, response, next) {
+  knex('books')
+    .insert({
+      title: request.body.title,
+      author: request.body.author,
+      genre: request.body.genre,
+      description: request.body.description,
+      cover_url: request.body.coverUrl
+    }, ['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+    .then((result) => {
+      response.set('Content-Type', 'application/json');
+      const sendResult = camelizeKeys(result);
+      response.send(sendResult[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/books/:id', function(request, response, next) {
+  let reqId = parseInt(request.params.id, 10);
+
+  if (!request.params.id || (isNaN(reqId))) {
+    return response.sendStatus(400);
+  } else if (reqId > 0) {
+    knex('books')
+      .where('id', reqId)
+      .first()
+      .then((result) => {
+        if (!result) {
+          return next();
+        }
+
+        return knex('books')
+          .update({
+            title: request.body.title,
+            author: request.body.author,
+            genre: request.body.genre,
+            description: request.body.description,
+            cover_url: request.body.coverUrl
+          }, ['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+          .where('id', reqId);
+      })
+      .then((result) => {
+        response.setHeader('Accept', 'application/json');
         const sendResult = camelizeKeys(result);
         response.send(sendResult[0]);
       })
@@ -58,35 +116,13 @@ router.get('/books/:id?', function (request, response, next) {
   };
 });
 
-router.post('/books', function(request, response, next) {
-  // const body = decamelizeKeys(request.body);
-  knex('books')
-    .insert({
-      title: request.body.title,
-      author: request.body.author,
-      genre: request.body.genre,
-      description: request.body.description,
-      cover_url: request.body.coverUrl
-    }, '*')
-    .then((result) => {
-      response.set('Content-Type', 'application/json');
-      response.send(result[0]);
-    })
-    .catch((err) => {
-      next(err);
-    });
+router.delete('/:id', function(request, response, next) {
+  
 });
-//
-// router.delete('/:id', function(request, response, next) {
-//   response.send(`Delete User - ${request.params.id}`);
-// });
-//
+
 // router.put('/:id', function(request, response, next) {
 //   response.send(`Update User - ${request.params.id}`);
 // });
 //
-// router.patch('/', function(request, response, next) {
-//   response.send(`Update User - ${request.params.id}`);
-// });
 
 module.exports = router;
