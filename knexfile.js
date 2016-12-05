49f5d6dc76b5979ae5481da9d559754eabf06dd0
CHANSE HORTON <chansehorton@yahoo.com>
2016-12-05 08:45:55 -0700

    correcting prod db connection knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://localhost/bookshelf_prod'
+    connection: 'postgres://tmosevuxjltkvq:fqKa4ed9OgeDlvuH9fPqN-GzUx@ec2-54-195-252-166.eu-west-1.compute.amazonaws.com:5432/d1ms7qf73r8d5g'
   }
 };
